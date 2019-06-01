a5246932204e67b2dae6ce17b530a7096346d65d
keitakn <keita.koga.work@gmail.com>
2019-06-01 17:32:01 +0900

    :sparkles: #1 ローカルでexpressサーバーを動作させるように実装

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import * as express from "express";
+import { Request, Response } from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import { eventContext } from 'aws-serverless-express/middleware';
+
+const app = express();
+
+app.use(compression());
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(eventContext());
+}
+
+app.get('/express', async (req: Request, res: Response) => {
+  const body = {
+    ip: req.ip,
+    baseUrl: req.baseUrl,
+  };
+
+  return res
+    .status(200)
+    .header('Content-Type', 'application/json')
+    .json(body);
+});
+
+export default app;
